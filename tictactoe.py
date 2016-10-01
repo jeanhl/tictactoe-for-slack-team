9f6ed27569622243c521ffd4befedbb30e1989a5
Jean Liu <hjliu415@gmail.com>
2016-09-30 18:27:29 -0700

    edge cases taken into account

--- tictactoe.py
+++ tictactoe.py
@@ -1,43 +1,78 @@
-def tictactoe():
-    board = new_board()
-    count = 1 
-    while count < 10:
-        input1 = (user_input() - 1)
+def play_tictactoe():
+    board = get_new_board()
+    move_count = 1 
+    max_turns = 10
+    print "~~~ WELCOME TO TIC TAC TOE!!! ~~~"
+    show_board(board)
+    while move_count < max_turns:
+        print current_user(move_count), "it's your turn."
+        input1 = get_user_input()
         if isinstance(board[input1], int):
-            board[input1] = current_user(count)
-            board_template(board)
-            if is_winner(count, board) is True:
+            board[input1] = current_symbol(move_count)
+            show_board(board)
+            if is_winner(move_count, board) is True:
                 break
-            count += 1 
+            move_count += 1 
         elif isinstance(board[input1], str):
             print "That spot is already taken"
-            board_template(board)
+            show_board(board)
         else:
             continue
    
-    if count == 10:        
+    if move_count == max_turns:        
         print "This is a draw."
     play_again()
 
 
-def new_board():
+def get_new_board():
     """ Sets up a new board for 1 game """
     return  [1, 2, 3,
              4, 5, 6,
              7, 8, 9]
 
-def current_user(count):
-    """ Switches turn. If count is odd, user1's turn. If count is 
+
+def show_board(board):
+    """ This prints the board """
+    print "\n\n     |     |     "
+    print " ", board[0], " | ", board[1], " | ", board[2], " "
+    print "-----+-----+-----" 
+    print " ", board[3], " | ", board[4], " | ", board[5], " "
+    print "-----+-----+-----"  
+    print " ", board[6], " | ", board[7], " | ", board[8], " "
+    print "     |     |     "
+
+
+def get_user_input():
+    input = raw_input("Pick a spot: ")
+    try:
+        int(input)
+    except ValueError:
+        print "Please type in a number"
+        return get_user_input()
+    if 1 <= int(input) <= 9:
+        return (int(input)-1)
+    else:
+        print "That spot is not on the board."
+        return get_user_input()
+
+
+def current_symbol(move_count):
+    """ Switches turn. If move_count is odd, user1's turn. If move_count is 
         even, user2's turn. """
 
-    if count % 2 != 0:
-        print "Player X, it's your turn"
+    if move_count % 2 != 0:
         return "X"
     else: 
-        print "Player O, it's your turn"
         return "O"
 
-def is_winner(count, board):
+
+def current_user(move_count):
+    """ Keeps track of current player. """
+
+    return "Player " + current_symbol(move_count)
+
+
+def is_winner(move_count, board):
     wins = [[0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
@@ -47,45 +82,24 @@ def is_winner(count, board):
             [0, 4, 8],
             [2, 4, 6]]
     for win in wins:
-        if ( board[win[0]] == current_user(count) and
-             board[win[1]] == current_user(count) and
-             board[win[2]] == current_user(count) ):
-            print "You win!!"
+        if (board[win[0]] == current_symbol(move_count) and
+            board[win[1]] == current_symbol(move_count) and
+            board[win[2]] == current_symbol(move_count)):
+            print "~`~`~`~`~",current_user(move_count), " winsss!!~`~`~`~`~"
             return True
         else:
             continue
 
 
-def board_template(board):
-    """ This prints the board """
-    print "     |     |     "
-    print " ", board[0], " | ", board[1], " | ", board[2], " "
-    print "-----+-----+-----" 
-    print " ", board[3], " | ", board[4], " | ", board[5], " "
-    print "-----+-----+-----"  
-    print " ", board[6], " | ", board[7], " | ", board[8], " "
-    print "     |     |     "
-
-
-def user_input():
-    # need to add in a check for strings 
-    input = int(raw_input("Pick a spot: "))
-    if 1 <= input <= 9:
-        return input
-    else:
-        print "That spot is not on the board."
-        return user_input()
-
 def play_again():
     """Asks players if they want to play again"""
     answer = raw_input("Would you like to play again? \n Y for yes > ")
-    if (answer == "Y" or answer == "Yes" or
-        answer == "y" or answer == "yes"):
-        tictactoe()
+    if (answer.lower() == "y" or answer.lower() == "yes"):
+        play_tictactoe()
     else:
         print "Thank you for playing!" 
 
 
 
 
-tictactoe()
+play_tictactoe()
