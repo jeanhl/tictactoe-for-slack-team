15008b2f6d48877cbfbca7ae61146c9a9f7b5466
Jean Liu <hjliu415@gmail.com>
2016-10-08 21:24:00 -0700

    Game now connects to Slack. Complete game is playable.

--- TT_Game_class.py
+++ TT_Game_class.py
@@ -1,10 +1,11 @@
 class TTT_Game(object):
 
-    def __init__(self, channel):
+    def __init__(self, channel, player1, player2):
         self.channel = channel
         self.player1 = player1
         self.player2 = player2
-        self.count = 1
+        self.turn_count = 1
+        self.max_turns = 10
         self.board = [1, 2, 3,
                       4, 5, 6,
                       7, 8, 9]
@@ -12,31 +13,33 @@ class TTT_Game(object):
 
     def get_formatted_board(self):
         """ Returns the board in the tictactoe format """
-        return ("```  {}  |  {}  |  {}\n".format(self.board[0], self.board[1], self.board[2]) +
+        return ("    {}  |  {}  |  {}\n".format(self.board[0], self.board[1], self.board[2]) +
             "-----+-----+-----\n" +
             "  {}  |  {}  |  {}\n".format(self.board[3], self.board[4], self.board[5]) +
             "-----+-----+-----\n" +
-            "  {}  |  {}  |  {}```".format(self.board[6], self.board[7], self.board[8])
+            "    {}  |  {}  |  {}".format(self.board[6], self.board[7], self.board[8])
             )
 
 
     def current_symbol(self):
         """ Switches turn. If move_count is odd, user1's turn. If move_count is 
             even, user2's turn. """
-        if self.count % 2 != 0:
+        if self.turn_count % 2 != 0:
             return "X"
         else: 
             return "O"
 
-    def current_user(self):
+    def current_player(self):
         """ Keeps track of current player. """
-        if self.count % 2 != 0:
+        if self.turn_count % 2 != 0:
+            print "IN METHOD", self.player1
             return self.player1
         else: 
+            print "IN METHOD", self.player2
             return self.player2
 
 
-    def is_winner(symbol, self):
+    def is_winner(self, symbol):
         """ Checks if the new entry on the board is a winner """
         wins = [[0, 1, 2],
                 [3, 4, 5],
@@ -50,7 +53,7 @@ class TTT_Game(object):
             if (self.board[win[0]] == symbol and
                 self.board[win[1]] == symbol and
                 self.board[win[2]] == symbol):
-                print "~`~`~`~`~ Player", symbol, " winsss!!~`~`~`~`~"
+                print "Game ends with a winner"
                 return True
             else:
                 continue
--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, request, Response
+import requests
+import json
+from flask import Flask, request, Response, jsonify
 from slackclient import SlackClient
-from pprint import pprint
+from TT_Game_class import TTT_Game
 app = Flask(__name__)
 
 SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
@@ -9,155 +11,152 @@ SLACK_TEST_TOKEN = os.environ.get('SLACK_TEST_TOKEN')
 SLACK_SLASH_TOKEN = os.environ.get('SLACK_SLASH_TOKEN')
 SC = SlackClient(SLACK_TEST_TOKEN)
 
-# to do to get 1 game going in 1 channel:
-# figure out how to make a game object with the following attributes:
-    
-# make a function to parse the incoming text to find the first username and return it
-# make a function to manage the dictionary to remember the game's history
-# edit the code so that it adds to the history, loads the current board and checks for winner 
+ALL_CHANNELS = {} #dictionary to keep track of all the game objects
 
 @app.route('/slack', methods=['POST'])
 def inbound():
     """ Manages requests from the Slack channel"""
     if request.form.get('token') == SLACK_SLASH_TOKEN:
-        channel = request.form.get('channel_name')
-        username = request.form.get('user_name')
-        text = request.form.get('text')
-        player2 = get_second_player(text, channel)
-        if player2 == None:
-            msg = "There is an error. Please check your request for typos. :/"
-            SC.api_call("chat.postMessage", as_user='false', channel=channel, text=msg)
-        test_chatmsg(channel)
-    return text + " and " + username + " are playing tictactoe in " + channel
+        channel = str(request.form.get('channel_name'))
+        player1 = str(request.form.get('user_name'))
+        text = str(request.form.get('text'))
+        response_url = str(request.form.get('response_url'))
+        checks_text_content(text, player1, channel, response_url)
+        return " "
 
-    # if I call a bunch of functions here, will there be an error? Synchronous processes?
 
-def test_chatmsg(channel):
-    """ testing to see if board shows up on Slack channel """
-    msg = show_board(get_new_board())
-    return SC.api_call("chat.postMessage", as_user='false', channel=channel, text=msg)
+def post_game_msg(msg, response_url, attachment=None):
+    """ Posts game msgs to Slack """
+    files = {"content-type": "application/json"}
+    chunk = {
+                "response_type": "in_channel",
+                "text": msg,
+                "attachments": [{"text": attachment}]
+            }
+    requests.request("POST", response_url, data=json.dumps(chunk), headers=files)
+
+
+def checks_text_content(text, player1, channel, response_url):
+    """ Checks the text of the slash command for:
+        If valid user: Make a new game
+        If integer: Make next move """
+    if len(text) == 0:
+        print "len = 0"
+        msg = "Doesn't seem like there is anything here :/"
+        post_game_msg(msg, response_url)
+    else:
+        print "len not 0"
+        player2 = get_second_player(text)
+        placement_num = check_if_valid_move(text)
+        if player2 is None and placement_num is None:
+            print "bad input"
+            msg = "I don't understand. Please check your request for typos."
+            post_game_msg(msg, response_url)
+        else:
+            if check_for_game_in_channel(channel) is True:
+                print "has a game"
+                if isinstance(placement_num, int):
+                    print "make a move"
+                    continue_game(ALL_CHANNELS[channel], placement_num, response_url, channel)
+                if isinstance(player2, str):
+                    print "already has game"
+                    msg = "There is already a game ongoing in this channel."
+                    post_game_msg(msg, response_url)
+            else:
+                print "no game"
+                if isinstance(placement_num, int):
+                    "tried to make amove"
+                    msg = "You are not currently in a game in this channel."
+                    post_game_msg(msg, response_url)
+                if isinstance(player2, str):
+                    "starting new game"
+                    start_new_game(channel, player1, player2, response_url)
+
 
 def get_all_users():
     """ Gets all the users in the team, including Slackbot. """
     list_of_users = []
-    dict_users = SC.api_call("users.list") # 
+    dict_users = SC.api_call("users.list")
     dict_members = dict_users["members"]
     for each in dict_members:
         list_of_users.append(each["name"])
     return list_of_users
 
-
-def get_second_player(text, channel):
+def get_second_player(text):
+    """ Checks if the user requested in the team """
     text = text.split()
-    for word in text:
-        if word in get_all_users():
-            return word
-        else:
-             return None
-
-
-def play_tictactoe():
-    board = get_new_board()
-    move_count = 1 
-    max_turns = 10
-    print "~~~ WELCOME TO TIC TAC TOE!!! ~~~"
-    show_board(board)
-    while move_count < max_turns:
-        print current_user(move_count), "it's your turn."
-        input1 = get_user_input()
-        if isinstance(board[input1], int):
-            board[input1] = current_symbol(move_count)
-            show_board(board)
-            if is_winner(current_symbol(move_count), board) is True:
-                break
-            move_count += 1 
-        elif isinstance(board[input1], str):
-            print "That spot is already taken"
-            show_board(board)
-        else:
-            continue
-   
-    if move_count == max_turns:        
-        print "This is a draw."
-    # play_again() #uncomment this line if want the option of asking to play again
-
-
-def get_new_board():
-    """ Sets up a new board for 1 game """
-    return  [1, 2, 3,
-             4, 5, 6,
-             7, 8, 9]
-
-
-def show_board(board):
-    """ This prints the board """
-    return ("```  {}  |  {}  |  {}\n".format(board[0], board[1], board[2]) +
-            "-----+-----+-----\n" +
-            "  {}  |  {}  |  {}\n".format(board[3], board[4], board[5]) +
-            "-----+-----+-----\n" +
-            "  {}  |  {}  |  {}```".format(board[6], board[7], board[8])
-            )
-
-
-def get_user_input():
-    input = raw_input("Pick a spot: ")
-    try:
-        int(input)
-    except ValueError:
-        print "Please type in a number"
-        return get_user_input()
-    if 1 <= int(input) <= 9:
-        return (int(input)-1)
+    if text[0][1:] in get_all_users():
+        return text[0][1:]
     else:
-        print "That spot is not on the board."
-        return get_user_input()
+        return None
 
-
-def current_symbol(move_count):
-    """ Switches turn. If move_count is odd, user1's turn. If move_count is 
-        even, user2's turn. """
-
-    if move_count % 2 != 0:
-        return "X"
-    else: 
-        return "O"
-
-
-def current_user(move_count):
-    """ Keeps track of current player. """
-
-    return "Player " + current_symbol(move_count)
-
-
-def is_winner(symbol, board):
-    """ Checks if the new entry on the board is a winner """
-    wins = [[0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]]
-    for win in wins:
-        if (board[win[0]] == symbol and
-            board[win[1]] == symbol and
-            board[win[2]] == symbol):
-            print "~`~`~`~`~ Player", symbol, " winsss!!~`~`~`~`~"
-            return True
+def check_if_valid_move(text, response_url=None):
+    """ Checks to see if text is an integer, if yes, turns it into a board location """
+    text = text.split()
+    try:
+        placement_num = int(text[0])
+        if 1 <= int(placement_num) <= 9:
+            return (int(placement_num)-1)
         else:
-            continue
+            msg = "That spot is not on the board."
+            post_game_msg(msg, response_url)
+    except ValueError:
+        return None
 
-# if want to play again, uncomment the section below
-# def play_again():
-#     """Asks players if they want to play again"""
-#     answer = raw_input("Would you like to play again? \n Y for yes > ")
-#     if (answer.lower() == "y" or answer.lower() == "yes"):
-#         play_tictactoe()
-#     else:
-#         print "Thank you for playing!" 
+
+def check_for_game_in_channel(channel):
+    """ Checks to see if there is an ongoing game in the channel. """
+    print "channel", channel
+    print "ALL_CHANNELS", ALL_CHANNELS
+    if channel in ALL_CHANNELS:
+        return True
+
+
+def start_new_game(channel, player1, player2, response_url):
+    """ Initiates a new game oject and announces a new game in the channel """
+    ALL_CHANNELS[channel] = TTT_Game(channel, player1, player2)
+    msg = ("~~~ WELCOME TO TIC TAC TOE!!! ~~~\n" + player1 + " has challenged " 
+            + player2 + " to a game of tictactoe in channel: " + channel + "!")
+    post_game_msg(msg, response_url, ALL_CHANNELS[channel].get_formatted_board())
+    msg = ALL_CHANNELS[channel].current_player() + ", it's your turn."
+    post_game_msg(msg, response_url)
+
+
+def continue_game(Current_Game, placement_num, response_url, channel):
+    """ Continues a game and states the next player's turn if needed """
+    play_tictactoe(Current_Game, placement_num, response_url, channel)
+    
+
+def play_tictactoe(Current_Game, placement_num, response_url, channel):
+    """ Runs the tic tac toe game specific to the channel """
+    print "in play tictactoe", placement_num
+    if Current_Game.turn_count < Current_Game.max_turns:
+        if isinstance(Current_Game.board[placement_num], int):
+            Current_Game.board[placement_num] = Current_Game.current_symbol()
+            post_game_msg("symbol places", response_url, Current_Game.get_formatted_board())
+            if Current_Game.is_winner(Current_Game.current_symbol()) is True:
+                msg = "We have a winner!!"
+                post_game_msg(msg, response_url, Current_Game.get_formatted_board())
+                return end_game(channel)
+            Current_Game.turn_count += 1
+            msg = Current_Game.current_player() + ", it's your turn."
+            post_game_msg(msg, response_url)
+        elif isinstance(Current_Game.board[placement_num], str):
+            msg = "That spot is already taken"
+            post_game_msg(msg, response_url, Current_Game.get_formatted_board())
+        else:
+            msg = "Error in the input"
+            post_game_msg(msg, response_url)
+    if Current_Game.turn_count == Current_Game.max_turns:
+        msg = "This is a draw."
+        post_game_msg(msg, response_url)
+        end_game(channel)
+
+def end_game(channel):
+    """ Deletes the game after it ends """
+    ALL_CHANNELS.pop(channel)
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
