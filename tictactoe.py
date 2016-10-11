1718d903e24912378375379b4c7aa50396e14055
Jean Liu <hjliu415@gmail.com>
2016-10-10 22:14:03 -0700

    ready to deploy

--- tictactoe.py
+++ tictactoe.py
@@ -342,6 +342,5 @@ def private_post_to_slack(msg, response_url, attachment=None):
 
 
 if __name__ == "__main__":
-    # PORT = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=PORT)
-    app.run()
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
